255250fec7f926f9b6cd2bc93a9d4549a96ad468
らけ <145347633+RAKE-FMS@users.noreply.github.com>
2024-06-24 11:09:28 +0900

    CSVファイル名の命名規則を設定
    ユーザー名、時刻をファイル名に含めるようになっている
    Objectのuser_nameで設定可能

--- Assets/Scripts/ExperimentTask2.cs
+++ Assets/Scripts/ExperimentTask2.cs
@@ -7,8 +7,7 @@ using System.Collections.Generic;
 
 public class ExperimentTask2 : MonoBehaviour
 {
-    public GameObject objectPrefab; // オブジェクトのプレハブ
-    private Vector3 initialPosition;
+    private Vector3 initialPosition;//ボールの初期位置
     public GameObject window; // ウィンドウ
     public float winFrontPos; // ウィンドウの手前の座標
     public float winBackPos; // ウィンドウの奥の座標
@@ -22,19 +21,16 @@ public class ExperimentTask2 : MonoBehaviour
 
     private int times = 1; // 今何回目のタスクか
 
-    public Vector3[] objectPositions; // オブジェクトの生成位置の配列
-    public Vector3[] windowPositions; // ウィンドウの生成位置の配列
+
     public float delayTime = 3f; // 静止時間
     public float stayDuration = 0.5f; // 静止判定の時間
     
-    private GameObject currentObject; // 現在のオブジェクト
-    private GameObject currentWindow; // 現在のウィンドウ
-    private bool isTaskStarted = false; // タスク開始フラグ
-    private bool isObjectMovable = false; // オブジェクト移動可能フラグ
     private Vector3 previousPosition; // 前回のオブジェクト位置
     private Stopwatch stayStopwatch = new Stopwatch(); // 静止判定用ストップウォッチ
     private Stopwatch delayStopwatch = new Stopwatch(); // 遅延時間用ストップウォッチ
     public Stopwatch taskStopwatch=new Stopwatch(); // 1タスクあたりのタイムスコアを計測するストップウォッチ
+    private DateTime TodayNow;//現在時間(CSVファイル名に使う)
+    public string user_name;//ファイル名に表示するユーザー名
 
     // CSV書き出し用
     private List<List<string>> data = new List<List<string>>();
@@ -65,24 +61,13 @@ public class ExperimentTask2 : MonoBehaviour
             task = UnityEngine.Random.Range(0, 2) + 2; // 2以上3未満の整数
             StartTask(task);
         }
+        else if (Input.GetKeyDown(KeyCode.Space) && (task == 2 || task == 3)) // スペースキーを押すとタイマーを再スタート
+        {
+            UnityEngine.Debug.Log("位置リセット+タイマーリスタート");
+            taskStopwatch.Restart();
+        }
         else if (Input.GetKeyDown(KeyCode.Escape)) EndTask();
-        //if (isTaskStarted)
-        //{
-        //    UpdateTaskState();
-        //    MoveObjectWithMouseInput();
-        //}
-        //else
-        //{
-        //    CheckDelayTimerToStartTask();
-        //}
-    }
-
-    private void SetupExperiment()
-    {
-        //SpawnObjectAndWindow();
-        //SetupObjectRigidbody();
-        //SetupWindowCollider();
-        //SetInitialWindowColor();
+        if (times > 20) EndTask(); //20タスク終了したらデータを記録
     }
 
     //タスクの開始
@@ -166,8 +151,13 @@ public class ExperimentTask2 : MonoBehaviour
     // 全タスクの終了
     private void EndTask()
     {
+        //時間を取得
+        TodayNow = DateTime.Now;
+        string filename = user_name+"_"+TodayNow.Year.ToString() + "_" + TodayNow.Month.ToString() + "_" + TodayNow.Day.ToString() + "_" + DateTime.Now.ToLongTimeString() + ".csv";
+        filename = filename.Replace(":", "_");
+        UnityEngine.Debug.Log(filename);
         // 書き出し先のファイルパス（プロジェクトフォルダのルートに書き出す）
-        string filePath = Path.Combine(Application.dataPath, "OutputData.csv");
+        string filePath = Path.Combine(Application.dataPath, filename);
 
         // CSVに書き出し
         WriteToCSV(filePath, data);
@@ -191,145 +181,8 @@ public class ExperimentTask2 : MonoBehaviour
         UnityEngine.Debug.Log("CSVファイルに書き出しました: " + filePath);
     }
 
-    //private void SpawnObjectAndWindow()
-    //{
-    //    int objectIndex = Random.Range(0, objectPositions.Length);
-    //    int windowIndex = Random.Range(0, windowPositions.Length);
-    //    currentObject = Instantiate(objectPrefab, objectPositions[objectIndex], Quaternion.identity);
-
-    //    //currentWindow = Instantiate(windowPrefab, windowPositions[windowIndex], Quaternion.identity);
-    //}
-
-    private void SetupObjectRigidbody()
-    {
-        Rigidbody objectRigidbody = currentObject.AddComponent<Rigidbody>();
-        objectRigidbody.useGravity = false;
-    }
-
-    private void SetupWindowCollider()
-    {
-        Collider windowCollider = currentWindow.AddComponent<BoxCollider>();
-        windowCollider.isTrigger = true;
-    }
-
-    private void SetInitialWindowColor()
-    {
-        currentWindow.GetComponent<Renderer>().material.color = Color.red;
-    }
-
     private void StartDelayTimer()
     {
         delayStopwatch.Start();
-    }
-
-    private void UpdateTaskState()
-    {
-        if (!isObjectMovable && IsDelayTimeElapsed())
-        {
-            isObjectMovable = true;
-            SetWindowColor(Color.red);
-        }
-    }
-
-    private bool IsDelayTimeElapsed()
-    {
-        return delayStopwatch.Elapsed.TotalSeconds >= delayTime;
-    }
-
-    private void SetWindowColor(Color color)
-    {
-        currentWindow.GetComponent<Renderer>().material.color = color;
-    }
-
-    private void MoveObjectWithMouseInput()
-    {
-        if (isObjectMovable && Input.GetMouseButton(0))
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0f;
-            currentObject.GetComponent<Rigidbody>().MovePosition(mousePosition);
-        }
-    }
-
-    private void CheckDelayTimerToStartTask()
-    {
-        if (IsDelayTimeElapsed())
-        {
-            isTaskStarted = true;
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (IsObjectEnteringWindow(other))
-        {
-            SetWindowColor(Color.green);
-            previousPosition = currentObject.transform.position;
-            stayStopwatch.Restart();
-        }
-    }
-
-    private bool IsObjectEnteringWindow(Collider other)
-    {
-        return other.gameObject == currentObject;
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (IsObjectStayingInWindow(other))
-        {
-            CheckObjectStationaryState();
-        }
-    }
-
-    private bool IsObjectStayingInWindow(Collider other)
-    {
-        return other.gameObject == currentObject;
-    }
-
-    private void CheckObjectStationaryState()
-    {
-        if (IsObjectStationary())
-        {
-            if (IsStayDurationElapsed())
-            {
-                UnityEngine.Debug.Log("Trial Clear!");
-                // 次の試行に移行するための処理を追加
-            }
-        }
-        else
-        {
-            UpdatePreviousPosition();
-            stayStopwatch.Restart();
-        }
-    }
-
-    private bool IsObjectStationary()
-    {
-        return currentObject.transform.position == previousPosition;
-    }
-
-    private bool IsStayDurationElapsed()
-    {
-        return stayStopwatch.Elapsed.TotalSeconds >= stayDuration;
-    }
-
-    private void UpdatePreviousPosition()
-    {
-        previousPosition = currentObject.transform.position;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (IsObjectExitingWindow(other))
-        {
-            SetWindowColor(Color.red);
-            stayStopwatch.Stop();
-        }
-    }
-
-    private bool IsObjectExitingWindow(Collider other)
-    {
-        return other.gameObject == currentObject;
     }
 }
